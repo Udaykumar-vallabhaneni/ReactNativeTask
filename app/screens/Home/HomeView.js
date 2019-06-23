3e9a46ac4a006e5305c9a12e73b60d9d02ead226
Uday Vallabhaneni <udayvallabhaneni@A2ML17041M.local>
2019-06-23 20:49:51 +0530

    Added Tab view

--- app/screens/Home/HomeView.js
+++ app/screens/Home/HomeView.js
@@ -2,10 +2,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
+import TabBar from 'react-native-underline-tabbar';
 import styles from './styles';
-import { ScrollView } from 'react-native-gesture-handler';
 import { getMainScreenData, selectMainScreenData } from '../../actions/apiActions';
+
+const Page = ({ label }) => {
+    return (<View style={[styles.container, { height: '100%' }]}>
+        <Text style={styles.welcome}>
+            {label}
+        </Text>
+        <Text style={styles.textFont}>Overview</Text>
+        <Text style={styles.textFont}>Repositeries <Text>11</Text></Text>
+        <Text style={styles.textFont}>Status <Text>6</Text></Text>
+        <Text style={styles.textFont}>Following</Text>
+    </View>);
+};
 
 class HomeView extends Component {
     constructor(props) {
@@ -18,59 +31,59 @@ class HomeView extends Component {
 
     // eslint-disable-next-line react/no-deprecated
     componentWillMount() {
-        this.props.fetchmainScreenData();
+        // this.props.fetchmainScreenData();
     }
 
 
     render() {
         return (
-            <View style={styles.container}>
-                <View style={styles.MainContainer}>
-                    {/*Donute Button Image */}
-                    <Image
-                        source={require('../../images/drawer.png')}
-                        style={styles.Image}
-                    />
-                    <Image
-                        source={require('../../images/gitIcon.png')}
-                        style={styles.CenterImage}
-                    />
-                    <Image
-                        source={require('../../images/notification.png')}
-                        style={styles.RightImage}
-                    />
-                </View>
-                <View style={styles.HeaderView}>
-                    <ScrollView>
-                        <Text>Overview</Text>
-                        <Text>Repositeries <Text>11</Text></Text>
-                        <Text>Status <Text>6</Text></Text>
-                        <Text>Following</Text>
-                    </ScrollView>
+            <View>
+                <View style={styles.MainHeader}>
+                    <View style={styles.MainContainer}>
+                        {/*Donute Button Image */}
+                        <Image
+                            source={require('../../images/drawer.png')}
+                            style={styles.Image}
+                        />
+                        <Image
+                            source={require('../../images/gitIcon.png')}
+                            style={styles.CenterImage}
+                        />
+                        <Image
+                            source={require('../../images/notification.png')}
+                            style={styles.RightImage}
+                        />
+                    </View>
+                    <View>
+                        {/* <ScrollView horizontal={true} style={styles.ScrollView}>
+                            <Text style={styles.textFont}>Overview</Text>
+                            <Text style={styles.textFont}>Repositeries <Text>11</Text></Text>
+                            <Text style={styles.textFont}>Status <Text>6</Text></Text>
+                            <Text style={styles.textFont}>Following</Text>
+                        </ScrollView> */}
+                    </View>
+                    <View style={styles.container}>
+                        <ScrollableTabView tabBarActiveTextColor="#53ac49" renderTabBar={() => <TabBar underlineColor="#53ac49" />}>
+                            <Page tabLabel={{ label: 'Overview', badge: 6 }} label="Overview" style={styles.tab} />
+                            <Page tabLabel={{ label: 'Repositeries', badge: 11 }} label="Repositeries" />
+                            <Page tabLabel={{ label: 'Status' }} label="Status" />
+                            <Page tabLabel={{ label: 'Following' }} label="Following" />
+                        </ScrollableTabView>
+                    </View>
                 </View>
             </View>
         );
     }
 }
 const mapStateToProps = state => ({
-    products: selectMainScreenData(state),
+    // products: selectMainScreenData(state),
 });
 
 function mapDispatchToProps(dispatch) {
     return {
-        fetchmainScreenData: () => dispatch(getMainScreenData()),
+        // fetchmainScreenData: () => dispatch(getMainScreenData()),
     };
 }
-
-// const withConnect = connect(mapStateToProps, mapDispatchToProps);
-
-// const withReducer = injectReducer({ key: 'homePage', reducer });
-// const withSaga = injectSaga({ key: 'homePage', saga });
-
-// export default connect(
-//     mapStateToProps,
-//     mapDispatchToProps
-// )(ProductView);
 
 export default connect(
     mapStateToProps,
--- app/screens/Home/styles.js
+++ app/screens/Home/styles.js
@@ -1,7 +1,9 @@
+/* eslint-disable react-native/no-color-literals */
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     CenterImage: {
+        // backgroundColor: '#fff',
         height: 25,
         justifyContent: 'center',
         marginLeft: 5,
@@ -9,6 +11,7 @@ const styles = StyleSheet.create({
     },
     HeaderView: {
         flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     Image: {
         height: 25,
@@ -19,16 +22,54 @@ const styles = StyleSheet.create({
     MainContainer: {
         flexDirection: 'row',
         flexWrap: 'wrap',
+        justifyContent: 'space-between',
         marginTop: 50,
     },
+    MainHeader: {
+        // backgroundColor: '#000',
+        height: 150
+    },
     RightImage: {
+        // backgroundColor: '#fff',
         height: 25,
         justifyContent: 'flex-end',
         marginLeft: 5,
         width: 25
     },
+    ScrollView: {
+        marginTop: 20,
+    },
     container: {
-    }
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        // flex: 1,
+        flex: 3,
+        height: 150,
+        justifyContent: 'center',
+    },
+    instructions: {
+        color: '#333333',
+        fontSize: 28,
+        marginBottom: 5,
+        textAlign: 'center',
+    },
+    tab: {
+        color: 'gray',
+    },
+    tabContainer: {
+        flex: 3,
+    },
+    textFont: {
+        // color: 'white',
+        fontSize: 15,
+        fontWeight: '400',
+        paddingLeft: 20
+    },
+    welcome: {
+        fontSize: 20,
+        margin: 10,
+        textAlign: 'center',
+    },
 });
 
 export default styles;
